f4fff070e1d4e9e0bda3ae6b789b257e92cd0d56
David Touitou <david.touitou.dt@gmail.com>
2019-08-15 13:05:41 +0200

    ex2 en cours, seed ok

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates_uniqueness_of :cocktail_id, :scope => [:ingredient_id]
+  validates :description, presence: true, allow_blank: false
+
+end
